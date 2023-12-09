b949bd4ba70953d96a194db8355e2742ca30f945
Mihai Iacov <mihaimxd@gmail.com>
2023-12-09 18:55:09 +0200

    Add Spring Docs generation - OpenAPI

--- src/main/java/com/store/management/StoreManagementToolApplication.java
+++ src/main/java/com/store/management/StoreManagementToolApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StoreManagementToolApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StoreManagementToolApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StoreManagementToolApplication.class, args);
+    }
 
 }
